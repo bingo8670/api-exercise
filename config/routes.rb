f7e7575b528721b4e9b3a0d818a7d20fd8eb02cc
bingo8670 <307286130@qq.com>
2017-07-28 23:21:37 +0800

    产生 API 用的 token

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'welcome#index'
+  
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
       get "/trains"  => "trains#index", :as => :trains
       get "/trains/:train_number" => "trains#show", :as => :train
- 
+
       get "/reservations/:booking_code" => "reservations#show", :as => :reservation
       post "/reservations" => "reservations#create", :as => :create_reservations
       patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
