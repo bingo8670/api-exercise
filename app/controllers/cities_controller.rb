749c9c5857aea88dbecebe573f8af1c9147130db
bingo8670 <307286130@qq.com>
2017-07-28 20:35:51 +0800

    更新城市天气

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,5 +2,17 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
-  
+
+  def update_temp
+    city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "4b1cf702cc5808f279056afadca920cf" }
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+
+  end
 end
