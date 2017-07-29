f2c3065b426b061c65f77ef924d7404d6961f9c8
bingo8670 <307286130@qq.com>
2017-07-29 10:41:17 +0800

    可以查询所有我的订票

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
-  
+
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
       get "/trains"  => "trains#index", :as => :trains
       get "/trains/:train_number" => "trains#show", :as => :train
+
+      get "/reservations" => "reservations#index", :as => :reservations
 
       get "/reservations/:booking_code" => "reservations#show", :as => :reservation
       post "/reservations" => "reservations#create", :as => :create_reservations
